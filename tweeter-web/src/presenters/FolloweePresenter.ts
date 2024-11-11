0191786e5e94d1657bb6620909136a07b094e83b
Jacob Williams <jawoba004@gmail.com>
2024-11-10 22:07:34 -0700

    got through the video tuturioals on phase 3

--- tweeter-web/src/presenters/FolloweePresenter.ts
+++ tweeter-web/src/presenters/FolloweePresenter.ts
@@ -1,15 +1,25 @@
 import { AuthToken, User } from "tweeter-shared";
 import { UserItemPresenter } from "./UserItemPresenter";
-import { PAGE_SIZE } from "./PagedItemPresenter";
+import { PAGE_SIZE, PagedItemView } from "./PagedItemPresenter";
+import { serverFacade } from "./../model/service/networkLayer/serverFacade";
 
 export class FolloweePresenter extends UserItemPresenter{
+
+    private facade: serverFacade;
+    
+    constructor(view: PagedItemView<User>) {
+      super(view);
+      this.facade = new serverFacade(); // Initialize serverFacade instance
+    }
+    
     protected getMoreItems(authToken: AuthToken, userAlias: string): Promise<[User[], boolean]> {
-      return this.service.loadMoreFollowees(
-        authToken,
+
+      return this.facade.getMoreFollowees({
+        token: authToken.token,
         userAlias,
-        PAGE_SIZE,
-        this.lastItem
-      );
+        pageSize: PAGE_SIZE,
+        lastItem: this.lastItem?.dto || null,
+      });
     }
     protected getItemDescription(): string {
       return "load followees";
