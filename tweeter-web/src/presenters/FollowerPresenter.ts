3f0f059d22052247b1e71083021ae821f92e26d7
Jacob Williams <jawoba004@gmail.com>
2024-11-11 21:53:51 -0700

    added follower to the backend

--- tweeter-web/src/presenters/FollowerPresenter.ts
+++ tweeter-web/src/presenters/FollowerPresenter.ts
@@ -1,17 +1,27 @@
 import { AuthToken, User } from "tweeter-shared";
 import { UserItemPresenter } from "./UserItemPresenter";
-import { PAGE_SIZE } from "./PagedItemPresenter";
+import { PAGE_SIZE, PagedItemView } from "./PagedItemPresenter";
+import { serverFacade } from "./../model/service/networkLayer/serverFacade";
 
 export class FollowerPresenter extends UserItemPresenter{
+
+    private facade: serverFacade;
+    
+    constructor(view: PagedItemView<User>) {
+      super(view);
+      this.facade = new serverFacade(); // Initialize serverFacade instance
+    }
+    
     protected getMoreItems(authToken: AuthToken, userAlias: string): Promise<[User[], boolean]> {
-     return this.service.loadMoreFollowers(
-        authToken,
+
+      return this.facade.getMoreFollowers({
+        token: authToken.token,
         userAlias,
-        PAGE_SIZE,
-        this.lastItem
-      );
+        pageSize: PAGE_SIZE,
+        lastItem: this.lastItem?.dto || null,
+      });
     }
     protected getItemDescription(): string {
       return "load followers";
     }
-}
+} 
